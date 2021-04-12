b589ebb4504b003d1b02aff578ac144c51636727
Ansah1234 <ansahpeter123@yahoo.com>
2021-04-12 18:38:20 +0100

    done for the day

--- middlewares/handleRouting.js
+++ middlewares/handleRouting.js
@@ -4,7 +4,9 @@ const handleRouting = async (req, res, next) => {
 	if (req.originalUrl === '/graphql' || req.originalUrl === '/') {
 		next();
 	} else {
+		//check code again
 		const shortener = req.originalUrl.split('/');
+		console.log(shortener);
 		const urlObject = await UrlModel.findOne({ where: { shortener } });
 		if (urlObject) {
 			//get original url
@@ -14,7 +16,7 @@ const handleRouting = async (req, res, next) => {
 			return;
 		}
 
-		res.write("url not found");
+		res.write('url not found');
 		res.end();
 	}
 };
