bb5f059f9e4bbec62ef6701160213d13551a8de2
Ansah1234 <ansahpeter123@yahoo.com>
2021-04-21 02:06:15 +0100

    charleyyyy

--- db/index.js
+++ db/index.js
@@ -4,7 +4,11 @@ const { Sequelize, DataTypes } = sql;
 export const sequelize = new Sequelize(
 	process.env.database,
 	process.env.username,
-	process.env.password
+	process.env.password,
+	{
+		host: process.env.host,
+		dialect: 'postgres',
+	}
 );
 
 export default DataTypes;
