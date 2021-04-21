a3e410deb507085dfa7aa4a44a989fbb896a289b
Ansah1234 <ansahpeter123@yahoo.com>
2021-04-21 01:53:38 +0100

    should work now

--- db/index.js
+++ db/index.js
@@ -1,8 +1,14 @@
 import sql from 'sequelize';
 
 const { Sequelize, DataTypes } = sql;
-export const sequelize = new Sequelize(process.env.DATABASE_URL, {
-	dialect: 'postgres',
-});
+export const sequelize = new Sequelize(
+	process.env.database,
+	process.env.username,
+	process.env.password,
+	{
+		host: process.env.host,
+		dialect: 'postgres',
+	}
+);
 
 export default DataTypes;
