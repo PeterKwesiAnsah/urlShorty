f283d9f35bd334b5f8d0995992186f7498d11db0
Ansah1234 <ansahpeter123@yahoo.com>
2021-04-21 01:23:15 +0100

    should work

--- db/index.js
+++ db/index.js
@@ -1,29 +1,8 @@
 import sql from 'sequelize';
-import PG from 'pg';
 
 const { Sequelize, DataTypes } = sql;
-// const client = new PG.Client('postgres://postgres:Engine1234@localhost:5432');
-
-// export const initdb = (async () => {
-// 	await client.connect();
-// 	await client.query('CREATE SCHEMA IF NOT EXISTS URLDB');
-// })();
-
-// const client = new pg.Connection(
-// 	'postgres://postgres:Engine1234@localhost:5432'
-// );
-// try{
-// 	client.query('CREATE DATABASE IF NOT EXISTS testdb')
-// }
-// catch(e){
-// 	console.log(e)
-// }
-
-export const sequelize = new Sequelize(
-	'postgres://postgres:Engine1234@localhost:5432/urlDB',
-	{
-		dialect: 'postgres',
-	}
-);
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+	dialect: 'postgres',
+});
 
 export default DataTypes;
