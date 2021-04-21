2b08be2229bd553ee08ccdcfdf84dc1666be9f74
Ansah1234 <ansahpeter123@yahoo.com>
2021-04-21 03:00:55 +0100

    again

--- db/index.js
+++ db/index.js
@@ -19,7 +19,10 @@ const { Sequelize, DataTypes } = sql;
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
 	dialect: 'postgres',
 	dialectOptions: {
-		ssl: true,
+		ssl: {
+			require: true,
+			rejectUnauthorized: false,
+		},
 	},
 });
 
